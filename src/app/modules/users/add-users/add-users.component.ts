7fd8fc64264c51d6f4636bf89ba2610037dbba4d
djaman <buslik@example.com>
2025-01-14 15:15:46 +0200

    TEST

--- src/app/modules/users/add-users/add-users.component.ts
+++ src/app/modules/users/add-users/add-users.component.ts
@@ -1,13 +1,33 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { DragDropDirective } from "@shared/directive/drag-drop.directive";
+import { InputFileComponent } from "@shared/components/input-file/input-file.component";
+import { MatButton } from "@angular/material/button";
+import { MatFormField, MatLabel } from "@angular/material/form-field";
+import { MatInput } from "@angular/material/input";
+import { FormBuilder, FormGroup, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-add-users',
   standalone: true,
-  imports: [],
+  imports: [
+    DragDropDirective,
+    InputFileComponent,
+    MatButton,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    ReactiveFormsModule
+  ],
   templateUrl: './add-users.component.html',
   styleUrl: './add-users.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AddUsersComponent {
+export class AddUsersComponent implements OnInit {
+  form: FormGroup<any>;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({});
+  }
 }
--- src/app/modules/users/users.routes.ts
+++ src/app/modules/users/users.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from "@angular/router";
 import { UsersDashboardComponent } from "@modules/users/users-dashboard/users-dashboard.component";
 import { EditUsersComponent } from "@modules/users/edit-users/edit-users.component";
 import { AddUsersComponent } from "@modules/users/add-users/add-users.component";
+import { isAdminGuard } from "@core/guards/is-admin.guard";
 
 
 export const userRoutes: Routes = [
@@ -15,11 +16,13 @@ export const userRoutes: Routes = [
     component: UsersDashboardComponent,
   },
   {
-    path: 'edit',
+    path: 'edit/:email',
     component: EditUsersComponent,
+    canActivate: [isAdminGuard],
   },
   {
     path: 'add',
     component: AddUsersComponent,
+    canActivate: [isAdminGuard],
   }
 ];
